fa98add6d27efa39ca830a48e0ad84af55ba8085
91tleg <maxchou0417@gmail.com>
2025-10-14 10:41:45 -0700

    refactor(i2c): use base class and gpio

--- src/drivers/i2c/i2c_config.h
+++ src/drivers/i2c/i2c_config.h
@@ -3,15 +3,14 @@
 
 #include <cstdint>
 #include "stm32h7xx.h"
+#include "../gpio/gpio_base.h"
 
 namespace drv {
 
 struct i2c_config_t
 {
-    GPIO_TypeDef* scl_port;
-    uint8_t scl_pin;
-    GPIO_TypeDef* sda_port;
-    uint8_t sda_pin;
+    gpio_base& scl;
+    gpio_base& sda;
     I2C_TypeDef* instance;
     uint8_t af;
 };
