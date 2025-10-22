2e4b198074382ebb50ca9375f962838ff98ce687
91tleg <maxchou0417@gmail.com>
2025-10-22 11:42:17 -0700

    refactor(ov7670): use static systick

--- src/drivers/camera/ov7670/ov7670.h
+++ src/drivers/camera/ov7670/ov7670.h
@@ -4,7 +4,6 @@
 #include <cstdint>
 #include "../../i2c/i2c_base.h"
 #include "../camera_base.h"
-#include "../../../common/systick/systick_base.h"
 #include "ov7670_config.h"
 
 namespace drv {
@@ -12,14 +11,15 @@ namespace drv {
 class ov7670 : public camera_base
 {
 public:
-    explicit ov7670(i2c_base& bus, ov7670_config_t& config, sys::systick_base& time);
+    explicit ov7670(i2c_base& bus, ov7670_config_t& config);
     void init() override;
     bool capture(uint8_t* buffer, uint32_t buf_size) override;
 
 private:
     i2c_base& _bus;
     ov7670_config_t& _config;
-    sys::systick_base& _time;
+    
+    void gpio_init();
     void write_reg(uint8_t reg, uint8_t value);
     void read_reg(uint8_t reg, uint8_t& value);
     uint8_t read_data_pins() const;
